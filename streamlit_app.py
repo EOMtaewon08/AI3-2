6b48075476bc60e5ee8499234e1d27f69de816a5
EOMtaewon08 <H098S02516@chunchon.gwe.hs.kr>
2025-11-23 11:58:04 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,8 +74,12 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
     #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
     # },
-}
-
+    
+    labels[0] : {"texts" : ["중국식 냉면은 맜있어"]},
+    labels[1] : {"texts" : ["짜장면은 맛있어"]},
+    labels[2] : {"texts" : ["짬뽕은 맜있어"]},
+    labels[3] : {"texts" : ["탕수육은 맜있어"]},
+    
 # ======================
 # 유틸
 # ======================
